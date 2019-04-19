ab9120938085265906ce111ec91512c1bd3d948f
kambuse <41556468+kambuse@users.noreply.github.com>
2019-04-20 01:10:21 +0800

    Add files via upload

---
+++ sqr.sh
@@ -0,0 +1,12 @@
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/menu.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/user-add.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/trial.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/hapus.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/user-login.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/user-list.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/delexp.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/resvis.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/speedtest_cli.py
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/info.sh
+wget https://raw.githubusercontent.com/kholizsivoi/script/master/about.sh
+
